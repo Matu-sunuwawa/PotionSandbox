cc263de7f0068102756650989e54c9360c9f8f4d
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-05-20 09:55:08 -0400

    configuration deploy on vercel

--- build_files.sh
+++ build_files.sh
@@ -2,18 +2,17 @@
 
 echo "BUILD START"
 
-# Ensure correct Python version is used
-python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip setuptools wheel
+# Ensure latest pip/setuptools
+python -m pip install --upgrade pip setuptools wheel
 
-# Install requirements
-python3.9 -m pip install -r requirements.txt
+# Install dependencies
+pip install -r requirements.txt
 
-# Create necessary directories
+# Create directories
 mkdir -p static
 mkdir -p staticfiles_build/static
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"

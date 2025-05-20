4ec206beeaa76e078d2eee31528415eeb700cc64
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-05-20 09:11:53 -0400

    configuration deploy on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+set -e  # Exit immediately if any command fails
 
 echo "BUILD START"
 
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.11 -m venv venv
+python -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
 
-# install all deps in the venv
+# install all dependencies in the virtual environment
 venv/bin/pip install -r requirements.txt
 
-# collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+# collect static files using the virtual environment's Python
+venv/bin/python manage.py collectstatic --noinput
 
 echo "BUILD END"
 
 # [optional] Start the application here 
-# python manage.py runserver
+# venv/bin/python manage.py runserver

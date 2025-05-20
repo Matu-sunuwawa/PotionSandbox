2c5a0261974aca964899035a92abf636c2d0430f
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-05-20 09:42:44 -0400

    configuration deploy on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 
 echo "BUILD START"
- python3.9 -m venv venv
- source venv/bin/activate
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+
+# Create static directory if it doesn't exist
+mkdir -p static
+
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"

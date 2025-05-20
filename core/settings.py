5ff1e0eb7549e54d507cc42647bf553ff445ca54
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-05-20 12:23:10 -0400

    configuration deploy on vercel

--- core/settings.py
+++ core/settings.py
@@ -164,6 +164,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ACCOUNT_NUMBER_START_FROM = 1000
 MINIMUM_DEPOSIT_AMOUNT = 10
 MINIMUM_WITHDRAWAL_AMOUNT = 10

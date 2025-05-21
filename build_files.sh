27626b49ec762d45c416bdedf0f6a7bf0b59ecee
Matu-sunuwawa <matyassinaadugna@gmail.com>
2025-05-21 08:55:02 -0400

    configuration deploy on vercel

--- build_files.sh
+++ build_files.sh
@@ -8,10 +8,6 @@ python -m pip install --upgrade pip setuptools wheel
 # Install dependencies
 pip install -r requirements.txt
 
-# Create directories
-mkdir -p static
-mkdir -p staticfiles_build/static
-
 # Collect static files
 python manage.py collectstatic --noinput --clear
 
